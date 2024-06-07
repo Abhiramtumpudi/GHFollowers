3c8e1985881fb36f4b91988c35264e2213c48c52
Abhiram <abhi.tumpudi@gmail.com>
2024-06-08 00:54:27 +0530

    added fullstop error file

--- GHFollwers/Utilities/ErrorMessages.swift
+++ GHFollwers/Utilities/ErrorMessages.swift
@@ -13,6 +13,6 @@ enum GFError : String, Error {
     case invalidUsername  = "Invalid username created is inValid Request . please check it."
     case unableTocomplete = "Verify throwing error while connecting to network."
     case responseError    = "check the server response from error . please try again."
-    case serverDataError  = "unable to convert data from server and decoding error."
+    case serverDataError  = "unable to convert data from server and decoding error.."
     
 }
